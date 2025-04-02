772caad9bb2f707ad8e4c37782081e7a8a186701
Aline Girardi <alinegirardiunb@gmail.com>
2025-04-02 19:06:07 -0300

    criando massa de dados

--- cypress/support/page_objects/produto.js
+++ cypress/support/page_objects/produto.js
@@ -3,16 +3,46 @@ class ProdutosPage {
     buscarProduto(nomeProduto, tamanho, cor ){
         //ações do método
         
-        
         cy.fixture('produtos').then(dados =>{
             cy.get('input[placeholder="Enter your search ..."]').eq(1).type(dados[0].nomeProduto)
             cy.get('.button-group >').eq(1).click()
-            //cy.get('.button-variable-item-').click(dados [0].tamanho)
-            //cy.get('.button-variable-item-').click(dados[0].cor)
-            //cy.get('.input-text').clear().type(quantidade) 
+            cy.get('.button-variable-item-' + dados[0].tamanho).click()
+            cy.get('.button-variable-item-' + dados[0].cor).click()
+            cy.get('.input-text').clear().type(dados[0].quantidade)
+            cy.get('.single_add_to_cart_button').click()
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
+        
+        cy.fixture('produtos').then(dados =>{
+            cy.get('input[placeholder="Enter your search ..."]').eq(1).type(dados[1].nomeProduto)
+            cy.get('.button-group >').eq(1).click()
+            cy.get('.button-variable-item-' + dados[1].tamanho).click()
+            cy.get('.button-variable-item-' + dados[1].cor).click()
+            cy.get('.input-text').clear().type(dados[1].quantidade)
+            cy.get('.single_add_to_cart_button').click()
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
         })
 
-        }
+        cy.fixture('produtos').then(dados =>{
+            cy.get('input[placeholder="Enter your search ..."]').eq(1).type(dados[2].nomeProduto)
+            cy.get('.button-group >').eq(1).click()
+            cy.get('.button-variable-item-' + dados[2].tamanho).click()
+            cy.get('.button-variable-item-' + dados[2].cor).click()
+            cy.get('.input-text').clear().type(dados[2].quantidade)
+            cy.get('.single_add_to_cart_button').click()
+            cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        })
+
+        cy.fixture('produtos').then(dados =>{
+            cy.get('input[placeholder="Enter your search ..."]').eq(1).type(dados[3].nomeProduto)
+            cy.get('.button-group >').eq(1).click()
+            cy.get('.button-variable-item-' + dados[3].tamanho).click()
+            cy.get('.button-variable-item-' + dados[3].cor).click()
+            cy.get('.input-text').clear().type(dados[3].quantidade)
+            cy.get('.single_add_to_cart_button').click()
+            cy.get('.woocommerce-message').should('contain', dados[3].nomeProduto)
+        })
+    }
 }
 
-export default new ProdutosPage()
+export default new ProdutosPage();
